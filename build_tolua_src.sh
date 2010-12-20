38b61d9a7cc222751c25a9b81b09d02fcc7d2c48
Tim Mensch <tim-gitpanther@bitgems.com>
2010-12-20 11:31:29 -0700

    Added some missing files.

---
+++ build_tolua_src.sh
@@ -0,0 +1,2 @@
+toluapp -C -H src/bin/toluabind.h -o src/bin/toluabind.c -n tolua src/bin/tolua_scons.pkg
+
---
+++ src/bin/toluabind.h
@@ -0,0 +1,8 @@
+/*
+** Lua binding: tolua
+** Generated automatically by tolua++-1.0.92 on 12/13/10 19:01:55.
+*/
+
+/* Exported function */
+TOLUA_API int  tolua_tolua_open (lua_State* tolua_S);
+
