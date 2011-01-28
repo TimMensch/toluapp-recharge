2e2d9af01e0fcee64af12c82db58b51ff01723c0
timmensch <tim-codaset@rechargegames.com>
2011-01-27 17:17:55 -0700

    Add the ability to have a "Lua object wrapper" class parameter for a function.

--- src/bin/toluabind.h
+++ src/bin/toluabind.h
@@ -1,6 +1,6 @@
 /*
 ** Lua binding: tolua
-** Generated automatically by tolua++-1.0.92 on 01/20/11 15:13:11.
+** Generated automatically by tolua++-1.0.92 on 01/27/11 17:10:45.
 */
 
 /* Exported function */
