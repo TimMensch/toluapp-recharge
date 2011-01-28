b2634da6aa0cb9e43bef21f9cb3e587267f2d241
timmensch <tim-codaset@rechargegames.com>
2011-01-27 19:03:10 -0700

    Regenerate toluabind.c/h; they were accidentally generated from the wrong source.

--- src/bin/toluabind.h
+++ src/bin/toluabind.h
@@ -1,6 +1,6 @@
 /*
 ** Lua binding: tolua
-** Generated automatically by tolua++-1.0.92 on 01/27/11 17:27:19.
+** Generated automatically by tolua++-1.0.92 on 01/27/11 19:01:29.
 */
 
 /* Exported function */
